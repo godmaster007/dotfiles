754c583cfd5b978626bb4c41a8e43821fd261c00
godmaster007 <default+default@gmail.com>
2019-04-27 21:28:59 -0700

    fixing bootstrap

--- home/bin/bootstrap_dotfiles.sh
+++ home/bin/bootstrap_dotfiles.sh
@@ -24,42 +24,42 @@
 # When forking, you can get the URL from the raw (<>) button.
 
 
-################################################################################
-# Set Github custom variables
-################################################################################
-# Github and SSH config
-echo "Hello, "$USER".  This script will configure your Github and SSH config."
+# ################################################################################
+# # Set Github custom variables
+# ################################################################################
+# # Github and SSH config
+# echo "Hello, "$USER".  This script will configure your Github and SSH config."
 
-# Username
-echo -n "Enter your Github Username (ex: gituser1234) and press [ENTER]: "
-read git_user
+# # Username
+# echo -n "Enter your Github Username (ex: gituser1234) and press [ENTER]: "
+# read git_user
 
-# Email
-echo -n "Enter your Github Email (ex: default@gmail.com) and press [ENTER]: "
-read git_email
+# # Email
+# echo -n "Enter your Github Email (ex: default@gmail.com) and press [ENTER]: "
+# read git_email
 
-# Repo
-echo -n "Enter your Github Repo (dotfiles.git) and press [ENTER]: "
-read git_repo
+# # Repo
+# echo -n "Enter your Github Repo (dotfiles.git) and press [ENTER]: "
+# read git_repo
 
-# These if's test if the variable is is null (has length 0)
-# This way if no input is entered it will auto setup my default config
-if [ -z $git_user ]; then
-	git_user='godmaster007'
-fi
+# # These if's test if the variable is is null (has length 0)
+# # This way if no input is entered it will auto setup my default config
+# if [ -z $git_user ]; then
+# 	git_user='godmaster007'
+# fi
 
-if [ -z $git_email ]; then
-	git_email='default+default@gmail.com'
-fi
+# if [ -z $git_email ]; then
+# 	git_email='default+default@gmail.com'
+# fi
 
-if [ -z $git_repo ]; then
-	git_repo='dotfiles.git'
-fi
+# if [ -z $git_repo ]; then
+# 	git_repo='dotfiles.git'
+# fi
 
-echo Your github username is: $git_user
-echo Your github email is: $git_email
-echo Your github reponame is: $git_repo
-echo Will set the remote url origin of your repo to: git@github.com:$git_user/$git_repo
+# echo Your github username is: $git_user
+# echo Your github email is: $git_email
+# echo Your github reponame is: $git_repo
+# echo Will set the remote url origin of your repo to: git@github.com:$git_user/$git_repo
 
 
 ################################################################################
@@ -101,30 +101,104 @@ xclip -sel clip < ~/.ssh/id_rsa.pub
 xdg-open "https://github.com/settings/ssh/new"
 
 
-################################################################################
-# Install homeshick (Adding another machine)
-################################################################################
-if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
-	git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-	source $HOME/.homesick/repos/homeshick/homeshick.sh
-fi
+ # Homeshick - Initial Install
+    # Github and SSH config
+    echo "Hello, "$USER".  This script will configure your Github and SSH config."
+
+    # Username
+    echo -n "Enter your Github Username (ex: gituser1234) and press [ENTER]: "
+    read git_user
+
+    # Email
+    echo -n "Enter your Github Email (ex: default@gmail.com) and press [ENTER]: "
+    read git_email
+
+    # Repo
+    echo -n "Enter your Github Repo (ex: dotfiles) and press [ENTER]: "
+    read git_repo
+
+    # These if's test if the variable is is null (has length 0)
+    # This way if no input is entered it will auto setup my default config
+    if [ -z $git_user ]; then
+      git_user='godmaster007'
+    fi
+
+    if [ -z $git_email ]; then
+      git_email='default+default@gmail.com'
+    fi
+
+    if [ -z $git_repo ]; then
+      git_repo='dotfiles'
+    fi
+
+    echo Your github username is: $git_user
+    echo Your github email is: $git_email
+    echo Your github reponame is: $git_repo
+    #echo Will set the remote url origin of your repo to: git@github.com:$git_user/"$git_repo".git
+    echo Will set the remote url origin of your repo to: https://www.github.com/"$git_user"/"$git_repo".git
+
+    # homeshick cd dotfiles
+    cd $HOME/.homesick/repos/dotfiles
+    git config --global user.email "$git_email"
+    git config --global user.name "$git_user"
+    git remote set-url origin git@github.com:"$git_user"/"$git_repo".git
+    cd ~
+
+    # Clone homeshick
+    git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+    
+    # Add homeshick to .bashrc, enable auto completion and auto refresh
+    printf '\n# Source
+    if [ -f ~/.homesick/repos/homeshick/homeshick.sh ]; then
+      source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+      source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
+    fi' >> $HOME/.bashrc
+    
+    # Enable Auto Refresh
+    printf '\n# Auto Refresh
+    homeshick refresh -q
+    \n' >> $HOME/.bashrc
+    
+    # Clone private dotfiles repo
+    if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
+      git clone https://www.github.com/"$git_user"/"$git_repo" $HOME/.homesick/repos/homeshick "$git_user"/"$git_repo"
+      source $HOME/.homesick/repos/homeshick/homeshick.sh
+    fi
+    
+    # Homeshick (HTTPS batch clone dotfiles to new machine)
+    # "--batch" bypasses user input questions like yes/no
+    # Cloning from the HTTPS link doesn't require SSH keys to be configured
+    homeshick --batch clone https://github.com/"$git_user"/"$git_repo".git
+    # May need to switch back to SSH git@github.com:godmaster007/dotfiles.git
+    source $HOME/.bashrc
+    source $HOME/.homesick/repos/homeshick/homeshick.sh
+    homeshick link --force
 
 
-################################################################################
-# Github Config - Custom Dotfiles
-################################################################################
-#Clone using batch to bypasses user input questions
-#homeshick --batch clone git@github.com:godmaster007/dotfiles.git
-# Changed to the https so it doesn't require authorization to clone
-homeshick --batch clone https://github.com/"$git_user"/"$git_repo"
-# Setup login identity for seamless updates and editing
-homeshick cd dotfiles
-git config --global user.email "$git_email"
-git config --global user.name "$git_user"
-git remote set-url origin git@github.com:"$git_user"/"$git_repo"
-cd ~
-# Link all files to $HOME
-homeshick link --force
+# ################################################################################
+# # Install homeshick (Adding another machine)
+# ################################################################################
+# if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
+# 	git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+# 	source $HOME/.homesick/repos/homeshick/homeshick.sh
+# fi
+
+
+# ################################################################################
+# # Github Config - Custom Dotfiles
+# ################################################################################
+# #Clone using batch to bypasses user input questions
+# #homeshick --batch clone git@github.com:godmaster007/dotfiles.git
+# # Changed to the https so it doesn't require authorization to clone
+# homeshick --batch clone https://github.com/"$git_user"/"$git_repo"
+# # Setup login identity for seamless updates and editing
+# homeshick cd dotfiles
+# git config --global user.email "$git_email"
+# git config --global user.name "$git_user"
+# git remote set-url origin git@github.com:"$git_user"/"$git_repo"
+# cd ~
+# # Link all files to $HOME
+# homeshick link --force
 
 
 ##########################
