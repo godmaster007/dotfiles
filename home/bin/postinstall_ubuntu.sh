36a7e31aa96479504380550ed7d23f513bca004e
godmaster007 <>
2026-06-14 13:01:31 -0700

    fix

--- home/bin/postinstall_ubuntu.sh
+++ home/bin/postinstall_ubuntu.sh
@@ -328,7 +328,7 @@ do
     
     9)
     ### Chrome Install - Browser
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    wget -qO- https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes -o /usr/share/keyrings/google-chrome.gpg
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
     #echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
     sudo apt update
